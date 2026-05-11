785c810b5305fc16ed6e26ad2dc3cf852eef7d24
MileHighHoosier <c2xt5q5fs2@privaterelay.appleid.com>
2026-05-11 12:28:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
+import os
+
 from flask import Flask
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
 @app.route("/")
 def home():
-    return "CastleWatch Backend Online"
+    try:
+        connection = engine.connect()
+        connection.close()
+
+        return "CastleWatch Backend + Database Online"
+
+    except Exception as e:
+        return f"Database Error: {str(e)}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
